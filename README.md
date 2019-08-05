114da841eb47673118fa1bcd4565f6fd8975932f
ratierd <ratierd@gmail.com>
2019-08-05 18:46:41 +0200

    Add terminal setup function

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Terminal Launch Config (VScode Extension)
 
 The purpose of this project is to dive into vscode extension development and eventually create an extension that allows a user to configure default terminal layout/commands depending on the selected vscode workspace.
+
+## .vscode/terminalConfig.json
+
+You can configure the terminals for a given workspace by adding a terminalConfig.json file in your project's .vscode directory like this :
+
+```
+{
+    "terminals": [
+        {
+            "command": "pwd",
+            "autorun": true
+        },
+        {
+            "command": "cd ~ && pwd",
+            "autorun": true
+        }
+    ]
+}
+```
--- src/extension.ts
+++ src/extension.ts
@@ -6,15 +6,27 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	var fs = require('fs');
-	
+
 	const workspaceFolders = vscode.workspace.workspaceFolders;
 	if (!workspaceFolders || !workspaceFolders.length) {
 		return;
 	}
 	const workspaceRoot = workspaceFolders[0];
-	
-	var obj = JSON.parse(fs.readFileSync(`${workspaceRoot.uri.fsPath}/.vscode/terminalConfig.json`, 'utf8'));
-	console.log(obj);
+
+	const { terminals } = JSON.parse(fs.readFileSync(`${workspaceRoot.uri.fsPath}/.vscode/terminalConfig.json`, 'utf8'));
+	setupTerminals(terminals);
+}
+
+function setupTerminals(terminals: any) {
+	if (!terminals || !terminals.length) {
+		return;
+	}
+	terminals.forEach((terminalConfig: { command: string; autorun: boolean; }) => {
+		const terminal = vscode.window.createTerminal({});
+		
+		// tslint:disable-next-line: no-unused-expression
+		terminalConfig.command && terminal.sendText(terminalConfig.command, terminalConfig.autorun);
+	});
 }
 
 // this method is called when your extension is deactivated
