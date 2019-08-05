9aa6452446ba6f7fd5dad0ded383f66e44537060
ratierd <ratierd@gmail.com>
2019-08-05 17:24:28 +0200

    Read terminalConfig json file

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,16 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	console.log("activated");
+	var fs = require('fs');
+	
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders || !workspaceFolders.length) {
+		return;
+	}
+	const workspaceRoot = workspaceFolders[0];
+	
+	var obj = JSON.parse(fs.readFileSync(`${workspaceRoot.uri.fsPath}/.vscode/terminalConfig.json`, 'utf8'));
+	console.log(obj);
 }
 
 // this method is called when your extension is deactivated
